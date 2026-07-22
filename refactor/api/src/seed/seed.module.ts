05867daffe3cc6ae3251e26434a4ffde60ea7fa4
razornez <razornez@gmail.com>
2026-07-22 14:04:19 +0700

    fix(api): register tenant repo in seed module

--- refactor/api/src/seed/seed.module.ts
+++ refactor/api/src/seed/seed.module.ts
@@ -4,11 +4,12 @@ import { Customer } from '../customers/entities/customer.entity';
 import { Faktur } from '../meter/entities/faktur.entity';
 import { HistoryMeter } from '../meter/entities/history-meter.entity';
 import { MeterPhoto } from '../meter/entities/meter-photo.entity';
+import { Tenant } from '../auth/entities/tenant.entity';
 import { SeedController } from './seed.controller';
 import { SeedService } from './seed.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer, Faktur, HistoryMeter, MeterPhoto])],
+  imports: [TypeOrmModule.forFeature([Customer, Faktur, HistoryMeter, MeterPhoto, Tenant])],
   controllers: [SeedController],
   providers: [SeedService],
 })
